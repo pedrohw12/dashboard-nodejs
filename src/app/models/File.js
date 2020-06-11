ba2d6c25a04893a6a20187b7f2c44fe16f3c119e
Pedro Wanderley <peu-zico>
2020-06-11 14:57:00 -0300

    first commit

---
+++ src/app/models/File.js
@@ -0,0 +1,27 @@
+import Sequelize, { Model } from "sequelize";
+
+// colocar apenas as colunas que serão inseriadas pelo prórprio usuário
+
+class File extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          },
+        },
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default File;
