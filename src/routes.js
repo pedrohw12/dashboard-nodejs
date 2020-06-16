0115e184f359b7a1f65269e609baa042369725e2
Pedro Wanderley <peu-zico>
2020-06-16 09:49:42 -0300

    appointments notification

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
 import AppointmentController from "./app/controllers/AppointmentController";
 import ScheduleController from "./app/controllers/ScheduleController";
+import NotificationController from "./app/controllers/NotificationController";
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,8 +23,12 @@ routes.put('/users', UserController.update);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
