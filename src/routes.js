4b58b6ca6d28ec4b5654a027b31266c746763714
Pedro Wanderley <peu-zico>
2020-06-15 21:41:44 -0300

    mongodb appointments

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
+import AppointmentController from "./app/controllers/AppointmentController";
+import ScheduleController from "./app/controllers/ScheduleController";
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,6 +19,11 @@ routes.post("/sessions", SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
