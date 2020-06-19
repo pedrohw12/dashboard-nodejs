6d33952a5b4f7da364c096b7600101ebb0b5a96d
Pedro Wanderley <peu-zico>
2020-06-19 10:55:59 -0300

    include user in schedule controller

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -25,7 +25,14 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
         },
       },
-      order: ['date'],
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["name"],
+        },
+      ],
+      order: ["date"],
     });
 
     return res.json(appointments);
